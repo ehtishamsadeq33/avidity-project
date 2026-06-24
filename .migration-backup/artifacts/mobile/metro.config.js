d0b0d908ac539869167dff31eeb0bcbc3af86aee
Ehtisham Mehmood Sadeq <ehtishamsadeq33@gmail.com>
2026-06-24 14:35:45 +0800

    Delete .migration-backup directory

--- .migration-backup/artifacts/mobile/metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-
-const config = getDefaultConfig(__dirname);
-
-config.resolver = config.resolver || {};
-config.resolver.blockList = /.*(?:@puppeteer[\\/]browsers|puppeteer-core|puppeteer)[\\/].*/;
-
-module.exports = config;
